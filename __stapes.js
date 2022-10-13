f2c18d28423d5ee4375fa856ce9363430313a848
Showrove Roy <showrove200@gmail.com>
2022-10-13 06:57:01 +0600

    site name and icon set

---
+++ __stapes.js
@@ -0,0 +1,12 @@
+/*
+https://github.com/programming-hero-web-course2/b6-quiz-crackerz-Showrove-Roy
+
+
+Get all quiz topics: https://openapi.programming-hero.com/api/quiz
+
+Get topic details by topic id: https://openapi.programming-hero.com/api/quiz/${id}
+
+Example: https://openapi.programming-hero.com/api/quiz/1
+
+
+*/
