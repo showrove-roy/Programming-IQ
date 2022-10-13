2643f12c3f887b00120fa70631c57f657a8b83d4
Showrove Roy <showrove200@gmail.com>
2022-10-13 18:06:01 +0600

    navBar responsive done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../Programming_IQ.png";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <nav className=' bg-[#06283D] h-12 flex align-middle justify-between  text-white font-semibold md:px-12'>
-      <div className='logo py-1'>
+    <nav className=' bg-[#06283D] h-12 flex align-middle justify-between  text-white font-semibold md:px-12 '>
+      <div className=' py-1 ml-8 z-50'>
         <Link to='/'>
           <img src={logo} alt='Programming_IQ' className='h-10' />
         </Link>
       </div>
-      <div className='navLink flex align-middle py-2'>
+      <div
+        className={`flex align-middle w-full justify-end py-2 absolute md:static duration-500 ease-in ${
+          open
+            ? "top-12 bg-[#06283D]  h-36 pb-5 border-t flex-col"
+            : "top-[-150px] bg-[#06283D] md:flex-row flex-col"
+        }`}>
         <NavLink
           to='/home'
           className='px-5 py-[2px] mx-2 hover:bg-[#F05454]  rounded-sm duration-500 '
@@ -43,6 +50,11 @@ const Header = () => {
           }>
           Blog
         </NavLink>
+      </div>
+      <div
+        onClick={() => setOpen(!open)}
+        className='h-10 w-10 flex align-middle cursor-pointer md:hidden  mr-8 z-50'>
+        {open ? <XMarkIcon /> : <Bars3Icon />}
       </div>
     </nav>
   );
