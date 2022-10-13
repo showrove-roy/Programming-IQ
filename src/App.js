e7d8a935c9a8f8aeeb4dbff59ac4f22b65231c6c
Showrove Roy <showrove200@gmail.com>
2022-10-13 07:04:07 +0600

    layout and router setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-
+import Main from "./layout/Main";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [],
+  },
+]);
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
