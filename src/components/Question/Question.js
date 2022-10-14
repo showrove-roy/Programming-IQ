1e0e972e0aaa36272932a1e4c9ed225b7bb948cf
Showrove Roy <showrove200@gmail.com>
2022-10-14 11:18:03 +0600

    quiz design done

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,11 +1,30 @@
 import React from "react";
 
-const Question = ({ qun }) => {
+const Question = ({ qun, index }) => {
   const { id, correctAnswer, options, question } = qun;
-
   return (
-    <div>
-      <h1>{question}</h1>
+    <div className=' rounded-md md:w-3/5 w-full mx-auto p-4 my-8 bg-[#DFF6FF]'>
+      <h1 className='md:text-xl text-lg font-medium my-4'>
+        <strong className='text-[#47B5FF]'>Quiz {index + 1}:</strong> {question}
+      </h1>
+      <div>
+        {options.map((option, indu) => (
+          <div
+            className='text-left pl-4 md:font-medium py-2 text-gray-600'
+            key={`${id}_${indu}`}>
+            <input
+              className='mr-2'
+              type='radio'
+              id={`${id}_${indu}`}
+              name={id}
+              value={option}
+            />
+            <label htmlFor={`${id}_${indu}`} className='cursor-pointer'>
+              {option}
+            </label>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Quizs/Quizs.js
+++ src/components/Quizs/Quizs.js
@@ -5,13 +5,20 @@ import Question from "../Question/Question";
 const Quizs = () => {
   const quizsData = useLoaderData();
 
-  const { id, logo, name, questions, total } = quizsData.data;
+  const { id, name, questions, total } = quizsData.data;
 
   return (
-    <div>
-      <h1>Quiz of {name}</h1>
-      {questions.map((qun) => (
-        <Question key={qun.id} qun={qun}></Question>
+    <div className='px-5'>
+      <h1 className='font-bold md:text-5xl text-3xl md:mt-10 mt-5 mb-2 text-[#F05454]'>
+        Quiz of {name}
+      </h1>
+      <hr className='h-1 md:w-40 w-28 m-auto mb-1 bg-[#30475E]' />
+      <hr className='h-1 md:w-32 w-20 m-auto md:mb-5 mb-2 bg-[#30475E]' />
+      <p className='md:mb-10 mb-5 text-2xl font-medium text-[#256D85]'>
+        Total Quiz: {total}
+      </p>
+      {questions.map((qun, index) => (
+        <Question key={qun.id} index={index} qun={qun}></Question>
       ))}
     </div>
   );
